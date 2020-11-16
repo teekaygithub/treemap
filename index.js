788aac158cdbb7ca45b481771ad34bb06bc59a70
teekaygithub <tomoyuki.kato@yahoo.com>
2020-11-16 10:13:56 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.html");
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+});
